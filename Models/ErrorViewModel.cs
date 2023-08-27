2bf41e629b40d067fa2e0dbbecab33cf816a3a28
gonzalo1998peru <gonzalo_huertas_oliva@outlook.es>
2023-08-27 16:12:54 -0500

    Archivos inciales

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace futbolperuano.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
